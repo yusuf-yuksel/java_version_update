7daded0b322a8281968f2a1815bb3ced6f2e1b29
yusuf <sebnemreklam@gmail.com>
2024-03-09 22:56:20 +0300

    stream task

---
+++ m6-stream/src/main/java/com/cydeo/task/DishTask.java
@@ -0,0 +1,38 @@
+package com.cydeo.task;
+
+public class DishTask {
+    public static void main(String[] args) {
+
+        //Print all dish's name that has less than 400 calories
+        DishData.getAll()
+                .stream()
+                .filter(p -> p.getCalories() < 400)
+                .map(dish -> dish.getName())
+                .forEach(System.out::println);
+
+        //Print the length of the name of each dish
+        DishData.getAll()
+                .stream()
+                .map(dish -> dish.getName())
+                .map(String -> String.length())
+                .forEach(System.out::println);
+
+        //Print three high caloric dish name (>300)
+        DishData.getAll()
+                .stream()
+                .filter(dish -> dish.getCalories()>300)
+                .limit(3)
+                .map(Dish::getName)
+                .forEach(System.out::println);
+
+        //Print all dish name that are below 400 calories in sorted
+        DishData.getAll()
+                .stream()
+                .filter(dish -> dish.getCalories()<=400)
+                .map(Dish::getName)
+                .sorted()
+                .forEach(System.out::println);
+
+
+    }
+}
