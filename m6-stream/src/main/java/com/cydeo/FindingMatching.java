d1727efff3c72cd004a14f4c0e9c8b723b970702
yusuf <sebnemreklam@gmail.com>
2024-03-10 22:01:31 +0300

    stream2

---
+++ m6-stream/src/main/java/com/cydeo/FindingMatching.java
@@ -0,0 +1,37 @@
+package com.cydeo;
+import com.cydeo.task.Dish;
+import com.cydeo.task.DishData;
+
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+
+public class FindingMatching {
+    public static void main(String[] args) {
+        boolean isHealth = DishData.getAll().stream()
+                .allMatch(dish -> dish.getCalories()<1000);
+        System.out.println(isHealth);
+
+        System.out.println("ANY MATCH");
+        if(DishData.getAll().stream().anyMatch(Dish::isVegetarian)){
+            System.out.println("The menu is vegetarian friendly");
+        }
+
+        System.out.println("NONE MATCH");
+        boolean isHealthy2 = DishData.getAll().stream().noneMatch(dish -> dish.getCalories() >= 1000);
+        System.out.println(isHealthy2);
+
+        System.out.println("FIND ANY");
+        Optional<Dish> dish = DishData.getAll().stream().filter(Dish::isVegetarian).findAny();
+        System.out.println(dish.get());
+
+        System.out.println("FIND FIRST");
+        Optional<Dish> dish2 = DishData.getAll().stream().filter(Dish::isVegetarian).findFirst();
+        System.out.println(dish2.get());
+
+        //parallel streams
+        System.out.println(IntStream.range(1,100).parallel().findAny());
+        System.out.println(IntStream.range(1,100).parallel().findFirst());
+
+    }
+}
