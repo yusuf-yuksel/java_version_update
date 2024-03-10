7e1fb8f143fbb6a465ba95b932f4d553570f1e67
yusuf <sebnemreklam@gmail.com>
2024-03-10 09:48:16 +0300

    stream task

--- m6-stream/src/main/java/com/cydeo/task/DishTask.java
+++ m6-stream/src/main/java/com/cydeo/task/DishTask.java
@@ -3,6 +3,8 @@ package com.cydeo.task;
 import java.util.Comparator;
 import java.util.stream.Stream;
 
+import static java.util.Comparator.comparing;
+
 public class DishTask {
     public static void main(String[] args) {
 
@@ -29,13 +31,17 @@ public class DishTask {
                 .forEach(System.out::println);
 
         //Print all dish name that are below 400 calories in sorted
+        System.out.println("--------------------------Task4-------------");
         DishData.getAll().stream()
                 .filter(dish -> dish.getCalories()<400)
-                .sorted(Comparator.comparing(Dish::getCalories))
+                .sorted(comparing(Dish::getCalories).reversed())
                 .map(Dish::getName)
                 .forEach(System.out::println);
 
-        Stream<Dish> dishStream = DishData.getAll().stream();
+
+
+
+
 
 
 
