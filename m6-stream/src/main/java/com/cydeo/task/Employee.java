40b81608df272ae5b7055c0c01b6fc287815e7e7
yusuf <sebnemreklam@gmail.com>
2024-03-10 10:21:46 +0300

    flatmap

---
+++ m6-stream/src/main/java/com/cydeo/task/Employee.java
@@ -0,0 +1,10 @@
+package com.cydeo.task;
+
+import java.util.List;
+
+public class Employee {
+    private int empId;
+    private String empName;
+    private String empEmail;
+    private List<String> empPhoneNumbers;
+  }
